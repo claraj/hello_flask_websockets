9913fbc57f0af2d9c2241aec2fb81e2781668490
clara <clara@clara.com>
2017-12-05 20:30:12 -0600

    requirements; /

--- static/client.js
+++ static/client.js
@@ -1,25 +1,26 @@
 //var socket = io.connect('https://' + document.domain + ':' + location.port);
 var socket = io.connect('https://thawing-depths-73556.herokuapp.com/socket.io')
 
-  socket.on('connect', function() {
-            socket.emit('hello', {data: 'I\'m connected!'});
-    });
+socket.on('connect', function() {
+  console.log('Connected to websocket server');
+  socket.emit('hello', {data: 'I\'m connected!'});
+});
 
 
-    var input = document.getElementById("new_message_text");
-    var button = document.getElementById("send_new_message");
-    var message_list = document.getElementById("message_list");
+var input = document.getElementById("new_message_text");
+var button = document.getElementById("send_new_message");
+var message_list = document.getElementById("message_list");
 
-    button.addEventListener("click", function() {
-      var text = input.value;
-      socket.emit("new_message", text);
-    })
+button.addEventListener("click", function() {
+  var text = input.value;
+  socket.emit("new_message", text);
+})
 
 
-    socket.on("new_message_received", function(new_message){
-      // add message to message_list
-      var li = document.createElement("li");
-      li.innerHTML = new_message;
-      message_list.append(li);
+socket.on("new_message_received", function(new_message){
+  // add message to message_list
+  var li = document.createElement("li");
+  li.innerHTML = new_message;
+  message_list.append(li);
 
-    })
+})
